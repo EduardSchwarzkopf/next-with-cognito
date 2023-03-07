8a5f2701f9c3a5b9da752d1686826c2b8a3fd4db
Eduard Schwarzkopf <eschwarzkopf@evoila.de>
2023-03-07 08:38:22 +0100

    provider no longer needed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,18 @@
 import "./globals.css";
-import Providers from "./providers";
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/*
+    return (
+        <html lang="en">
+            {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+            <head />
+            <body>{children}</body>
+        </html>
+    );
 }
